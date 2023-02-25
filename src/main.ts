7b3e3aa5510abf3fd23a10895db054d2b72b9ab9
markdturner <mark.turner@newcastle.ac.uk>
2023-02-25 11:40:48 +0000

    switched to SVG for better click detection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import './style.css'
-import imgUrl from './images/backgrounds/background.png'
+import background from './images/backgrounds/background.svg'
+
+let stageWidth: number = 1920,
+    stageHeight: number = 1080
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="h-screen bg-contain bg-no-repeat bg-center" style="background-image: url('${imgUrl}')">
-
+  <div id="background" class="drop-shadow-lg aspect-video">
+    <embed id="stage" src="${background}"></embed>
   </div>
 `
+
+function init() {
+  console.log(background)
+
+  stageHeight = document.querySelector<HTMLDivElement>('#background')!.offsetHeight
+  stageWidth = document.querySelector<HTMLDivElement>('#background')!.offsetWidth
+
+  console.log(stageHeight, stageWidth)
+}
+
+document.querySelector<HTMLDivElement>('#stage')!.addEventListener('load', () => {
+  const stage = document.querySelector<HTMLDivElement>('#stage')
+  const vegetablePatch = getContentDocument(stage).querySelector('g#vegetablePatch')
+
+  vegetablePatch.addEventListener('click', (event: PointerEvent) => {
+    console.log(event)
+  })
+
+})
+
+
+function getContentDocument(embeddingElement: any) {
+  if (embeddingElement.contentDocument) {
+    return embeddingElement.contentDocument;
+  }
+  try {
+    return embeddingElement.getSVGDocument();
+  } catch (e) {}
+  return null;
+}
+
+init()
