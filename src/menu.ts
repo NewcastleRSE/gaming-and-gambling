ab132c8af6263d50b9a3cafb31de416a1e16cedc
markdturner <mark.turner@newcastle.ac.uk>
2023-02-27 21:36:08 +0000

    menu event handlers

--- src/menu.ts
+++ src/menu.ts
@@ -1,19 +1,44 @@
 import home from './images/buttons/home.png'
 import game from './images/buttons/shovel.png'
 import inventory from './images/buttons/case.png'
-import settings from './images/buttons/settings.png'
+import info from './images/buttons/info.png'
 import sound from './images/buttons/speaker.png'
 import odds from './images/buttons/tree.png'
 
 export function init() {
     document.querySelector<HTMLDivElement>('#menu')!.innerHTML = `
     <div class="grid place-items-center gap-4 grid-cols-6">
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${home}')" />
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${game}')" />
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${inventory}')" />
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${settings}')" />
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${sound}')" />
-        <button class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${odds}')" />
-    </div>
-    `
+        <button id="homeBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${home}')" />
+        <button id="gameBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${game}')" />
+        <button id="inventoryBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${inventory}')" />
+        <button id="infoBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${info}')" />
+        <button id="soundBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${sound}')" />
+        <button id="oddsBtn" class="rounded-4 w-16 h-16 background-cover" style="background-image: url('${odds}')" />
+    </div>`
+
+    const menu = document.querySelector<HTMLDivElement>('#menu')
+
+    menu!.querySelector<HTMLDivElement>('#homeBtn')!.addEventListener('click', () => {
+        console.log('home')
+    })
+
+    menu!.querySelector<HTMLDivElement>('#gameBtn')!.addEventListener('click', () => {
+        console.log('game')
+    })
+
+    menu!.querySelector<HTMLDivElement>('#inventoryBtn')!.addEventListener('click', () => {
+        console.log('inventory')
+    })
+
+    menu!.querySelector<HTMLDivElement>('#infoBtn')!.addEventListener('click', () => {
+        console.log('info')
+    })
+
+    menu!.querySelector<HTMLDivElement>('#soundBtn')!.addEventListener('click', () => {
+        console.log('sound')
+    })
+
+    menu!.querySelector<HTMLDivElement>('#oddsBtn')!.addEventListener('click', () => {
+        console.log('odds')
+    })
 }
