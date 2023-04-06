c387a66b6b3003f88604a3e3c19a1eedf057b728
Mark Turner <mark.turner@ncl.ac.uk>
2023-04-06 12:12:50 +0100

    basic timer in footer

--- src/footer.ts
+++ src/footer.ts
@@ -1,16 +1,27 @@
 import * as Utils from './utils'
+import Timer from 'easytimer.js'
 
-const footer = document.querySelector<HTMLDivElement>('#footer')
+const timer = new Timer()
 
 export function init() {
     return Utils.htmlToElement(
     `<div class="w-1/2 mx-auto p-4 modal border-8 rounded-3xl flex text-white">
         <div class="w-1/3 text-center"><span id="clickCount">0</span> Clicks</div>
-        <div class="w-1/3 text-center">Timer</div>
+        <div class="w-1/3 text-center"><span id="timer">00:00:00</span></div>
         <div class="w-1/3 text-center">Rarity</div>
     </div>`)
 }
 
+timer.addEventListener('secondsUpdated', function (event) {
+    document.querySelector<HTMLDivElement>('#timer')!.innerHTML = timer.getTimeValues().toString()
+})
+
 export function updateClickCount() {
-    document.querySelector<HTMLDivElement>('#clickCount')!.innerText = (Number(document.querySelector<HTMLDivElement>('#clickCount')!.innerText) + 1).toString()
+    const counter = (Number(document.querySelector<HTMLDivElement>('#clickCount')!.innerText) + 1)
+
+    if(counter === 1) {
+        timer.start()
+    }
+
+    document.querySelector<HTMLDivElement>('#clickCount')!.innerText = counter.toString()
 }
